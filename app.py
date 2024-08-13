e74ef2389a600f281654fa406f904c0d561174cf
Angel Baez <angi020101@gmail.com>
2024-08-13 19:58:46 -0400

    otra

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,19 @@ def contacto():
 @app.route('/contacto2')
 def contacto2():
     return render_template('contacto.html')
+
+@app.route('/guardar-cliente',methods=['POST'])
+def guardarCliente():
+    print(request.form)
+    nombrecliente = request.form.get('nombre')
+    mailcliente = request.form.get('email')
+    telefonocliente = request.form.get('telefono')
+    direccioncliente = request.form.get('direccion')
+    return f"""<h3>Cliente Registrado:</h3>
+            <p><strong>Nombre:</strong> {nombrecliente}</p>
+            <p><strong>Email:</strong> {mailcliente}</p>
+            <p><strong>Teléfono:</strong> {telefonocliente}</p>
+            <p><strong>Dirección:</strong> {direccioncliente}</p>"""
 
 if __name__=='__main__':
     app.run(debug=True)
